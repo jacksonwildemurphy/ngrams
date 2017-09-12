65fb270e91995172f9c53d7c10a596e15f10b27b
Jackson Murphy <jackson@Jacksons-MacBook-Air.local>
2017-09-12 13:12:27 -0600

    unsmooted bigram language model is working

--- ngrams.py
+++ ngrams.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 
-# adds a tokenized array to the unigram frequency table
+# adds tokens from an array to the unigram frequency table
 def add_to_uni_table(token_arr):
     for t in token_arr:
         if t in uni_frequency_table:
@@ -9,7 +9,7 @@ def add_to_uni_table(token_arr):
         else:
             uni_frequency_table[t] = 1
 
-# adds a tokenized array to the bigram frequency table, prepending "phi" to beginning of sentence
+# adds tokens from an array to the bigram frequency table, prepending "phi" to beginning of sentence
 def add_to_bi_table(token_arr):
     token_arr.insert(0, "phi") # prepend "phi" to beginning of sentence
     for i in range(len(token_arr) - 1):
@@ -20,8 +20,8 @@ def add_to_bi_table(token_arr):
             bi_frequency_table[bigram] = 1
 
 # Returns the conditional probability (w_i | w_iMinus1)
-def prob_wi_given_w_iMinus1(w_i, w_iMinus1):
-    bigram = w_i + " " + w_iMinus1
+def conditional_prob_unsmoothed(w_i, w_iMinus1):
+    bigram = w_iMinus1 + " " + w_i
     if(bigram not in bi_frequency_table):
         return 0
     prob_wi_AND_wiMinus1 = bi_frequency_table[bigram] / bi_frequencies_sum
@@ -32,6 +32,18 @@ def prob_wi_given_w_iMinus1(w_i, w_iMinus1):
     prob_wiMinus1 = freq_w_iMinus1 / bi_frequencies_sum
     return (prob_wi_AND_wiMinus1 / prob_wiMinus1)
 
+def conditional_prob_smoothed(w_i, w_iMinus1):
+    bigram = w_iMinus1 + " " + w_i
+    if(bigram not in bi_frequency_table):
+        return 0
+    prob_wi_AND_wiMinus1 = bi_frequency_table[bigram] / bi_frequencies_sum
+    freq_w_iMinus1 = 0 # initialize
+    for k,v in bi_frequency_table.items(): # count how many times w_iMinus1 is first part of a bigram
+        if k.split()[0] == w_iMinus1:
+            freq_w_iMinus1 += v
+    prob_wiMinus1 = freq_w_iMinus1 / bi_frequencies_sum
+    return (prob_wi_AND_wiMinus1 / prob_wiMinus1)
+    
 # Calculates and prints the log-probability of a given sentence using the unigram language model
 def print_sentence_prob_unigram(sentence, uni_frequencies_sum):
     token_arr = sentence.lower().split()
@@ -42,7 +54,7 @@ def print_sentence_prob_unigram(sentence, uni_frequencies_sum):
 
     print("Unsmoothed Unigrams, logprob(S) = " + "{0:.4f}".format(logprob_sum))
 
-# Calculates and prints the log-probability of a given sentence using the unigram language model
+# Calculates and prints the log-probability of a given sentence using the bigram language model
 def print_sentence_prob_bigram_unsmoothed(sentence, bi_frequencies_sum):
     token_arr = sentence.lower().split()
     token_arr.insert(0, "phi") # prepend "phi" to beginning of sentence
@@ -51,18 +63,35 @@ def print_sentence_prob_bigram_unsmoothed(sentence, bi_frequencies_sum):
     for i in range(1, len(token_arr)):
         w_i = token_arr[i]
         w_iMinus1 = token_arr[i - 1]
-        prob = prob_wi_given_w_iMinus1(w_i, w_iMinus1)
+        prob = conditional_prob_unsmoothed(w_i, w_iMinus1)
         if prob == 0:
             print("Unsmoothed Bigrams, logprob(S) = undefined")
             return
-        logprob = np.log2(prob_wi_given_w_iMinus1(w_i, w_iMinus1))
-        logprob_sum += token_prob
+        logprob = np.log2(prob)
+        logprob_sum += logprob
 
     print("Unsmoothed Bigrams, logprob(S) = " + "{0:.4f}".format(logprob_sum))
+
+# Calculates and prints the log-probability of a given sentence using the bigram language model
+# Applies an add-one smoothing
+def print_sentence_prob_bigram_smoothed(sentence, bi_frequencies_sum):
+    token_arr = sentence.lower().split()
+    token_arr.insert(0, "phi") # prepend "phi" to beginning of sentence
+    logprob_sum = 0
+
+    for i in range(1, len(token_arr)):
+        w_i = token_arr[i]
+        w_iMinus1 = token_arr[i - 1]
+        prob = conditional_prob_smoothed(w_i, w_iMinus1)
+        logprob = np.log2(prob)
+        logprob_sum += logprob
+
+    print("Smoothed Bigrams, logprob(S) = " + "{0:.4f}".format(logprob_sum))
 
 ### START OF PROGRAM ###
 if(len(sys.argv) < 4):
     print("This program requires 3 commandline arguments: <training-file> -test <test-file> \n Please try again")
+    sys.exit()
 
 uni_frequency_table = {}
 bi_frequency_table = {}
@@ -89,14 +118,5 @@ for sentence in test_file:
     print("S = ",sentence)
     print_sentence_prob_unigram(sentence, uni_frequencies_sum)
     print_sentence_prob_bigram_unsmoothed(sentence, bi_frequencies_sum)
-
+    #print_sentence_prob_bigram_smoothed(sentence, bi_frequencies_sum)
     print()
-
-
-
-
-#print(bi_frequency_table)
-
-            # print ("This is the name of the script: ", sys.argv[0])
-            # print ("Number of arguments: ", len(sys.argv))
-            # print ("The arguments are: " , str(sys.argv))
